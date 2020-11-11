8cba4c42c478c23eaaa818225e23a5197559ac02
Saurabh M <saurabh85mahajan@gmail.com>
2020-11-11 18:33:09 +0530

    Ajax Pagination

--- app/Http/Livewire/Items.php
+++ app/Http/Livewire/Items.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Item;
+use Livewire\WithPagination;
 
 class Items extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.items');
+        $items = Item::where('user_id', auth()->user()->id)
+            ->paginate(10);
+
+        return view('livewire.items', [
+            'items' => $items
+        ]);
     }
 }
--- resources/views/livewire/items.blade.php
+++ resources/views/livewire/items.blade.php
@@ -1,3 +1,44 @@
-<div>
-    Coming from the Livewire Component
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div class="mt-8 text-2xl">
+        Items
+    </div>
+
+    <div class="mt-6">
+        <table class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">ID</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">Name</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        <div class="flex items-center">Price</div>
+                    </th>
+                    <th class="px-4 py-2">
+                        Status
+                    </th>
+                    <th class="px-4 py-2">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($items as $item)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $item->id}}</td>
+                        <td class="border px-4 py-2">{{ $item->name}}</td>
+                        <td class="border px-4 py-2">{{ number_format($item->price, 2)}}</td>
+                        <td class="border px-4 py-2">{{ $item->status ? 'Active' : 'Not-Active'}}</td>
+                        <td class="border px-4 py-2">Edit Delete</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="mt-4">
+        {{ $items->links() }}
+    </div>
 </div>
